506c437fefdb1a997a73fb46325090dc1a184b6e
Calvin Thomas <calvinthomas@protonmail.com>
2026-02-23 23:25:55 +0000

    Initial base set-up with Initializr for backend
    Initial set-up via Initializr, configured to have required dependencies and added SpringDocs with basic configuration.

---
+++ church-finance-backend/src/main/kotlin/com/calvin_tech/church_finance/ChurchFinanceApplication.kt
@@ -0,0 +1,11 @@
+package com.calvin_tech.church_finance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChurchFinanceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChurchFinanceApplication>(*args)
+}
